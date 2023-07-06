95924541e538dbfe854b66823290ea8cb05241fa
Mihail Gaberov <mihail.gaberov@olx.com>
2023-07-06 16:24:14 +0200

    :alembic: Experimenting with a slot machine

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import SlotMachine from '../components/SlotMachine.svelte';
+</script>
+
+<main>
+  <h1>Caesar's Game</h1>
+  <SlotMachine />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    margin-top: 50px;
+  }
+</style>
